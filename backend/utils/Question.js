6120122ee1e9c96e6a7929423f79b5b908527a62
mavericksijan <101683965+mavericksijan@users.noreply.github.com>
2023-06-13 20:05:34 +1000

    Add files via upload

---
+++ backend/utils/Question.js
@@ -0,0 +1,56 @@
+export default[
+    {
+        id:1,
+        question:"What year was the very first model of the iPhone released?",
+         options:[
+            '2006',
+            '2007',
+            '2008',
+            '2009',
+        ]
+    },
+    {
+        id:2,
+        question:"What's the shortcut for the \"copy\" function on most computers",
+        options:[
+            'ctrl + p',
+            'ctrl + n',
+            'ctrl + c',
+            'ctrl + v',
+
+        ]
+
+    },
+    {
+        id:3,
+        question:"What is often seen as the smallest unit of memory?",
+        options:[
+            'Kilobyte',
+            'Byte',
+            'Megabyte',
+            'Terabyte',
+        ]
+    },
+    {
+        id:4,
+        question:"Which of the following is a type of OS?",
+        options:[
+            'Java',
+            'Python',
+            'Windows',
+            'C',
+        ]
+
+    },
+    {
+        id:5,
+        question:"Who is often called the father of the computer?",
+        options:[
+            'Charles Babbage',
+            'Charles Gabbage',
+            'Bill Gates',
+            'Sijan Shrestha',
+        ]
+    }
+];
+export const answer = [1, 2, 1, 2, 0];
